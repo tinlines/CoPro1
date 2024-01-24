5859cccd472907a6188363e7fd74e158cba96af0
tinlines <157444184+tinlines@users.noreply.github.com>
2024-01-24 10:44:50 +0800

    Update ForLoop6.java

--- ForLoop6.java
+++ ForLoop6.java
@@ -1,15 +1,12 @@
-package FirstSem;
-
 public class ForLoop6 {
 
 	public static void main(String[] args) {
 		for (int i = 1; i <= 10; ++i) {
-
-            // if the value of i is 5 the loop terminates  
-            if (i == 5) {
-                break;
-            }      
-            System.out.println(i);
-        }   
-    }
+			// if the value of i is 5 the loop terminates  
+			if (i == 5) {
+				break;
+			}      
+			System.out.println(i);
+		}   
+	}
 }
