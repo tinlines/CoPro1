076c871ded74da0f5a086439906ca9a44f9059a8
tinlines <157444184+tinlines@users.noreply.github.com>
2024-01-24 08:46:28 +0800

    Update ForLoop4.java

--- ForLoop4.java
+++ ForLoop4.java
@@ -1,16 +1,12 @@
-package FirstSem;
-
 public class ForLoop4 {
 
 	public static void main(String[] args) {
 		int sum = 0;
-	    int n = 10;
-	    //for displaying sum of numbers
-	    for (int i = 1; i <= n; ++i) {
-	      
-	      sum += i;     // sum = sum + i
-	    }
-	       
-	    System.out.println("Sum = " + sum);
-	  }
+		int n = 10;
+		for (int i = 1; i <= n; ++i) {
+			sum += i;     // sum = sum + i
+			System.out.println(sum);//displaying numbers
+		}
+		System.out.println("Sum = " + sum);//for displaying sum of numbers
 	}
+}
