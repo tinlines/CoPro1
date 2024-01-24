7656ce6e5ed718e481d647b5c9ad43a7f6c317fe
tinlines <157444184+tinlines@users.noreply.github.com>
2024-01-24 08:23:13 +0800

    Update Array1.java

--- Array1.java
+++ Array1.java
@@ -1,4 +1,3 @@
-package FirstSem;
 import java.util.Scanner;
 public class Array1 {
 
@@ -26,13 +25,30 @@ public class Array1 {
 		System.out.print("Enter 1-6 to select a name: ");
 		num = s.nextInt();
 		switch(num) {
-		case 1:
-			System.out.println("The initials for " + a + " is " + name[0].charAt(0) + name[1].charAt(0) + ".");
-			System.out.println("Suggested username: " + a.replace(' ', '_').toLowerCase());
-			break;
+			case 1:
+				System.out.println("The initials for " + a + " is " + name[0].charAt(0) + name[1].charAt(0) + ".");
+				System.out.println("Suggested username: " + a.replace(' ', '_').toLowerCase());
+				break;
+			case 2:
+				System.out.println("The initials for " + b + " is " + name[0].charAt(0) + name[2].charAt(0) + ".");
+				System.out.println("Suggested username: " + b.replace(' ', '_').toLowerCase());
+				break;
+			case 3:
+				System.out.println("The initials for " + c + " is " + name[1].charAt(0) + name[0].charAt(0) + ".");
+				System.out.println("Suggested username: " + c.replace(' ', '_').toLowerCase());
+				break;
+			case 4:
+				System.out.println("The initials for " + d + " is " + name[1].charAt(0) + name[2].charAt(0) + ".");
+				System.out.println("Suggested username: " + d.replace(' ', '_').toLowerCase());
+				break;
+			case 5:
+				System.out.println("The initials for " + e + " is " + name[2].charAt(0) + name[0].charAt(0) + ".");
+				System.out.println("Suggested username: " + e.replace(' ', '_').toLowerCase());
+				break;
+			case 6:
+				System.out.println("The initials for " + f + " is " + name[2].charAt(0) + name[1].charAt(0) + ".");
+				System.out.println("Suggested username: " + f.replace(' ', '_').toLowerCase());
+				break;
 		}
-		
-
 	}
-
 }
