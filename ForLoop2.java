aa74b2110d3cf86dbee01efccf015e841a85e1fe
tinlines <157444184+tinlines@users.noreply.github.com>
2024-01-24 08:34:36 +0800

    Update ForLoop2.java

--- ForLoop2.java
+++ ForLoop2.java
@@ -1,14 +1,10 @@
-package FirstSem;
-
 public class ForLoop2 {
 
 	public static void main(String[] args) {
-		int num = 3;
+		int num = 5;
 		//printing
 		for(int i = 0; i <= num; i++) {
-			System.out.println(i); //System.out.println(i); to display number
+			System.out.println(i); //System.out.println(i); to display the number
 		}
-
 	}
-
 }
