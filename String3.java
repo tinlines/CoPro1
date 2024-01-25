82343b084bec2214d654cb9ea9a44876a2d34ead
tinlines <157444184+tinlines@users.noreply.github.com>
2024-01-25 09:04:30 +0800

    Update String3.java

--- String3.java
+++ String3.java
@@ -1,12 +1,8 @@
-package FirstSem;
-
 public class String3 {
 
 	public static void main(String[] args) {
 		String sentence = "The quick brown fox jumps over the lazy dog.";
 		sentence = sentence.replace(' ', '%');
 		System.out.println(sentence);
-
 	}
-
 }
