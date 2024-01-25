ade0c1aad53eb84a8f27802a5d359103c0be6777
tinlines <157444184+tinlines@users.noreply.github.com>
2024-01-25 09:03:15 +0800

    Update String2.java

--- String2.java
+++ String2.java
@@ -1,11 +1,9 @@
-package FirstSem;
-
 public class String2 {
 
 	public static void main(String[] args) {
 		String str1 = "Class", str2 = " class ";
 		
-		if(str1.equalsIgnoreCase(str2.trim())) {
+		if(str1.equalsIgnoreCase(str2.trim())) { //since it's on .equalsIgnoreCase, the output will be true otherwise false
 			System.out.println("True");
 		}
 		else {
