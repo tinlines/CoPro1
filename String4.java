54dddf55f05fae1fdd7c355480d8e7f68c54b317
tinlines <157444184+tinlines@users.noreply.github.com>
2024-01-25 09:08:50 +0800

    Update String4.java

--- String4.java
+++ String4.java
@@ -1,12 +1,8 @@
-package FirstSem;
-
 public class String4 {
 
 	public static void main(String[] args) {
 		String sentence = "The quick brown fox jumps over the lazy dog.";
-		int letter = sentence.indexOf("h");
-		System.out.println("Index " + letter);
-
+		int letter = sentence.indexOf("h"); 
+		System.out.println("Index " + letter); //the index start at zero so the index of char h in sentence is 1 while the T is 0
 	}
-
 }
