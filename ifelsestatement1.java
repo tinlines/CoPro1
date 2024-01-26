0471d49f3c528d0691fd49a85a8a77625a01e0c6
tinlines <157444184+tinlines@users.noreply.github.com>
2024-01-26 11:56:48 +0800

    Update ifelsestatement1.java

--- ifelsestatement1.java
+++ ifelsestatement1.java
@@ -1,15 +1,14 @@
-package FirstSem;
-
+import java.util.Scanner;
 public class ifelsestatement1 {
 
 	public static void main(String[] args) {
-		int grade = 75;
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter your grade: ");
+		int grade = s.nextInt();
 		if(grade >= 75) {
 			System.out.println("Passed");
 		}else {
 			System.out.println("Failed");
 		}
-
 	}
-
 }
