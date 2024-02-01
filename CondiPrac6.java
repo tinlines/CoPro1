f801515138cfe18809499f3883fee3ab2adcfe7b
tinlines <157444184+tinlines@users.noreply.github.com>
2024-02-01 11:48:50 +0800

    Update CondiPrac6.java

--- CondiPrac6.java
+++ CondiPrac6.java
@@ -1,13 +1,13 @@
-package Condi;
 import java.util.Scanner;
 public class CondiPrac6 {
-
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.print("Month : ");
 		int month = s.nextInt();
+		
 		System.out.print("Date  : ");
 		int date = s.nextInt();
+		
 		System.out.print("Year  : ");
 		int year = s.nextInt();
 		
