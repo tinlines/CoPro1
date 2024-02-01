69e4fc4dd3c4398e0abce8a3cbf32f02d757bd57
tinlines <157444184+tinlines@users.noreply.github.com>
2024-02-01 11:36:55 +0800

    Update ArithmeticOperators.java

--- ArithmeticOperators.java
+++ ArithmeticOperators.java
@@ -1,22 +1,12 @@
-package arithmeticOperators;
-
 import java.util.Scanner;
-
 public class ArithmeticOperators {
-
 	public static void main(String[] args) {
-	
-		
 		Scanner scan = new Scanner(System.in);
-		
 		String name; int age;
+		
 		System.out.print("Enter Name: ");
-		
 		name = scan.nextLine();
-		
-		 
-		
+			
 		System.out.println("Your name is " + name);
 	}
-
 }
